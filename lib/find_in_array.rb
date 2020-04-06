50b7b1cdb6bb7fe1ac3c9822b1b554fb346d58a5
Injae Lee <ilee.injae@gmail.com>
2020-04-06 04:49:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,10 +2,9 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do
     if array[count] == value_to_find 
-    array[count]
+    count 
   else 
     nil 
     count += 1
   end 
-array  
 end
